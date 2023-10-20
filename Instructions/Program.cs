ae61209527b6b3373670943e7845d873e38dc861
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-10-20 09:15:06 +0300

    exercise with for

--- Instructions/Program.cs
+++ Instructions/Program.cs
@@ -4,14 +4,57 @@
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Number=");
+            string numberAsString = Console.ReadLine();
+
+            int number = int.Parse(numberAsString);
+            for (int i = 1; i <= number; i++)
+            {
+                // block
+                bool isOdd = i % 2 == 0;
+                if (isOdd)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+        public static bool EvaluateCondition(bool condition, string message)
+        {
+            Console.WriteLine(message);
+            return condition;
+        }
+
+        public static void SamplesWithIf()
+        {
+            if (EvaluateCondition(false, "Eval condition1"))
+            {
+                Console.WriteLine("Condition1 is true");
+            }
+            else if (EvaluateCondition(false, "Eval condition2"))
+            {
+                Console.WriteLine("Condition2 is true");
+            }
+            else if (EvaluateCondition(true, "Eval condition3"))
+            {
+                Console.WriteLine("Condition3 is true");
+            }
+            else
+            {
+                Console.WriteLine("None of Condition1, Condition2, Condition3 are true");
+            }
+        }
+
+        public static void SamplesWitSwitch()
+        {
             Console.WriteLine("Meniu:");
             Console.WriteLine("1) File");
             Console.WriteLine("2) Edit");
             Console.WriteLine("3) View");
             Console.Write("Your option is:");
             string option = Console.ReadLine();
-            
-
 
             int optionNumber;
             if (int.TryParse(option, out optionNumber))
@@ -55,34 +98,6 @@
             {
                 Console.WriteLine("Please enter a valid numeric option.");
             }
-
-            
-
-
-            if (EvaluateCondition(false, "Eval condition1"))
-            {
-                Console.WriteLine("Condition1 is true");
-            }
-            else if (EvaluateCondition(false, "Eval condition2"))
-            {
-                Console.WriteLine("Condition2 is true");
-            }
-            else if (EvaluateCondition(true, "Eval condition3"))
-            {
-                Console.WriteLine("Condition3 is true");
-            }
-            else
-            {
-                Console.WriteLine("None of Condition1, Condition2, Condition3 are true");
-            }
-
-            Console.WriteLine("Hello, World!");
-        }
-
-        public static bool EvaluateCondition(bool condition, string message)
-        {
-            Console.WriteLine(message);
-            return condition;
         }
     }
 }
