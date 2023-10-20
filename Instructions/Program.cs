76f47f16e5c2810bc850a4c5a74c4c5743432862
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-10-20 08:35:56 +0300

    samples with if

---
+++ Instructions/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Instructions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (EvaluateCondition(false, "Eval condition1"))
+            {
+                Console.WriteLine("Condition1 is true");
+            }
+            else if (EvaluateCondition(false, "Eval condition2"))
+            {
+                Console.WriteLine("Condition2 is true");
+            }
+            else if (EvaluateCondition(true, "Eval condition3"))
+            {
+                Console.WriteLine("Condition3 is true");
+            }
+            else
+            {
+                Console.WriteLine("None of Condition1, Condition2, Condition3 are true");
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static bool EvaluateCondition(bool condition, string message)
+        {
+            Console.WriteLine(message);
+            return condition;
+        }
+    }
+}
